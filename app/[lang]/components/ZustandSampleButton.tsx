4c7f06d22b074fd54dc14e927454ea07c7786d6f
rivernine <rivernine@github.com>
2023-10-18 20:48:51 +0900

    ⚰️ Remove dead code

--- app/[lang]/components/ZustandSampleButton.tsx
+++ app/[lang]/components/ZustandSampleButton.tsx
@@ -6,7 +6,7 @@ export default function ZustandSampleButton() {
   return (
     <button
       type="button"
-      className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       onClick={() => {
         useStore.setState((state) => ({
           title: state.title + 'append',
