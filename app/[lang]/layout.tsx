484d8cb49ede374bde28613aea642a50866099ef
rivernine <rivernine@github.com>
2023-10-21 12:00:08 +0900

    🔍 Generate dynamic i18n metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -6,17 +6,36 @@ import { Noto_Sans_KR } from 'next/font/google'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import { getCountriesKeyObj } from '@/libs/utils/country';
+import { getDictionary } from '@/i18n/get-dictionary';
 
+/**
+ * 폰트 정의
+ */
 const notoSansKR = Noto_Sans_KR({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Nextjs Swc Zustand App Router',
-  description: 'This is template.',
-  icons: {
-    icon: "/favicon.ico"
+/**
+ * 다이나믹 메타데이터 생성
+ */
+export async function generateMetadata({
+  params: { lang }
+}: {
+  params: any
+}): Promise<Metadata> {
+  const dic = await getDictionary(lang);
+  const title = dic.Metadata.title;
+  const description = dic.Metadata.description;
+  return {
+    title,
+    description,
+    icons: {
+      icon: "/favicon.ico"
+    }
   }
 }
 
+/**
+ * 파라미터 생성
+ */
 export function generateStaticParams() {
   return getCountriesKeyObj();
 }
